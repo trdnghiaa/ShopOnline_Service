60c141648440bf3c7fac0b731e3f1983b5309b17
Tran Nghia <tran.dainghia7122003@gmail.com>
2024-06-23 20:37:57 +0700

    login and register

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true }
+});
+
+// Hash mật khẩu trước khi lưu vào MongoDB
+userSchema.pre('save', async function (next) {
+  try {
+    // Nếu mật khẩu đã được thay đổi hoặc mới tạo, hash lại mật khẩu
+    if (this.isModified('password') || this.isNew) {
+      const hashedPassword = await bcrypt.hash(this.password, 10);
+      this.password = hashedPassword;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Phương thức so sánh mật khẩu
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
